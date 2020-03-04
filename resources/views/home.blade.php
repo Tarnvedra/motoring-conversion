b1a3abe9392d30955746d96516c0e610e60e01db
Paul Colley <drguru.track79@yahoo.co.uk>
2020-03-04 13:35:41 +0000

    image adjust again!..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,44 +5,44 @@
     <div class="text-center pb-3"><h1>Motoring Conversion Dashboard</h1></div>
    <div class="row">
     <div class="d-flex pt-1">
-        <div class="pr-5"><a href="{{url('/distance')}}"><img src="public/img/length.svg" height="150" width="150"></a>
+        <div class="pr-5"><a href="{{url('/distance')}}"><img src="img/length.svg" height="150" width="150"></a>
         <figcaption>Distance</figcaption></div>
         <div class="pr-5"><a href="{{url('/volume')}}"><img src="img/volume.svg" height="150" width="150"></a>
         <figcaption>Volume</figcaption></div>
-        <div><a href="{{url('/mass')}}"><img src="img/weight.svg" height="200" width="200"></a>
+        <div><a href="{{url('/mass')}}"><img src="img/weight.svg" height="150" width="150"></a>
         <figcaption>Mass</figcaption></div>
     </div>
     </div>
 
     <div class="row">
     <div class="d-flex pt-1">
-        <div class="pr-5"><a href="{{url('/force')}}"><img src="img/force.svg" height="200" width="200"></a>
+        <div class="pr-5"><a href="{{url('/force')}}"><img src="img/force.svg" height="150" width="150"></a>
         <figcaption>Force</figcaption></div>
-        <div class="pr-5"><a href="{{url('/pressure')}}"><img src="img/pressure.svg" height="200" width="200"></a>
+        <div class="pr-5"><a href="{{url('/pressure')}}"><img src="img/pressure.svg" height="150" width="150"></a>
         <figcaption>Pressure</figcaption></div>
-        <div><a href="{{url('/torque')}}"><img src="img/torque.svg" height="200" width="200"></a>
+        <div><a href="{{url('/torque')}}"><img src="img/torque.svg" height="150" width="150"></a>
         <figcaption>Torque</figcaption></div>
     </div>
     </div>
 
     <div class="row">
     <div class="d-flex pt-1">
-        <div class="pr-5"><a href="{{url('/power')}}"><img src="img/power.svg"height="200" width="200" ></a>
+        <div class="pr-5"><a href="{{url('/power')}}"><img src="img/power.svg"height="150" width="150" ></a>
         <figcaption>Power</figcaption></div>
-        <div class="pr-5"><a href="{{url('/velocity')}}"><img src="img/speed.svg" height="200" width="200"></a>
+        <div class="pr-5"><a href="{{url('/velocity')}}"><img src="img/speed.svg" height="150" width="150"></a>
         <figcaption>Velocity</figcaption></div>
-        <div><a href="{{url('/consumption')}}"><img src="img/fuel.svg" height="200" width="200"></a>
+        <div><a href="{{url('/consumption')}}"><img src="img/fuel.svg" height="150" width="150"></a>
         <figcaption>Fuel Consumption</figcaption></div>
     </div>
     </div>
 
     <div class="row">
         <div class="d-flex pt-1">
-            <div class="pr-5"><a href="{{url('/temperature')}}"><img src="img/temperature.svg" height="200" width="200"></a>
+            <div class="pr-5"><a href="{{url('/temperature')}}"><img src="img/temperature.svg" height="150" width="150"></a>
             <figcaption>Temperature</figcaption></div>
-            <div class="pr-5"><a href="{{url('/electrical')}}"><img src="img/electrical.svg" height="200" width="200"></a>
+            <div class="pr-5"><a href="{{url('/electrical')}}"><img src="img/electrical.svg" height="150" width="150"></a>
             <figcaption>Electrical</figcaption></div>
-            <div><a href="{{url('/retrieve')}}"><img src="img/retrieve.svg" height="200" width="200" ></a>
+            <div><a href="{{url('/retrieve')}}"><img src="img/retrieve.svg" height="150" width="150" ></a>
             <figcaption>Retrieve</figcaption></div>
         </div>
     </div>
