5d888c3094723f26428e5f4aa68980d2e7ba1e3a
Paul Colley <drguru.track79@yahoo.co.uk>
2020-05-23 11:10:12 +0100

    updated distance.blade.php

--- resources/views/distance.blade.php
+++ resources/views/distance.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="text-center pb-1"><h1>Distance Calculation</h1></div>
-    <div class="text-center pb-1"><h5>* All calculation results are approximate values and should treated as such *</h5></div>
+    <div class="text-center pb-1"><h5>* All calculation results are approximate values and should be treated as such *</h5></div>
     
     <calculation-form></calculation-form>
     
